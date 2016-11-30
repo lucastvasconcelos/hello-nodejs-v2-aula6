73b584fc9f72c5597d4dbb8926a6fd0533e7f60c
lucastvasconcelos <lucastavaresvasconcelos@gmail.com>
2016-11-30 20:06:34 -0300

    utilizando jquery e promessas

--- appweb/index.js
+++ appweb/index.js
@@ -2,25 +2,23 @@
 const express    = require("express");
 const bodyParser = require("body-parser");
 const app        = express();
-
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(bodyParser.json());
 app.post("/dosave", (req,res) => {
   console.log("os dados recebidos do formulário são:");
   console.log(req.body);
-  res.send(`<h1>Sucesso!</h1><ul><li>Seu nome: ${req.body.nome}</li><li>Endereço: ${req.body.endereco} </li><li>Telefone: ${req.body.telefone} </li></ul>
-<ahref='index.html'>voltar</a>`);
+  res.send("<h1>Sucesso!</h1><a href='index.html'>voltar</a>");
 });
-
-app.post("/adivinha",(req,res) =>{
- 	console.log("numero: ");
- 	console.log(req.body.numero);
- 	res.send(`<h1> ${req.body.numero} </h1>
-	<a href="numero.html"> voltar </a>
-`)
+app.post("/adivinha", (req,res) => {
+  if(req.body && req.body.numero == 7)
+    res.send("<h1>Acertou!</h1><a href='numero.html'>voltar</a>");
+  else
+    res.send("<h1>Errou!</h1><a href='numero.html'>voltar</a>");
 });
-
-app.listen(3005);
+app.get("/convidados", (req,res) => {
+  var convs = [{nomeconvidado:"Maria"},{nomeconvidado:"João"}];
+  res.send(convs);
+});
+app.listen(3000);
 console.log("app online");
