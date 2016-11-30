210e707c7f7d09c448cd33ecbbe4a076b6bceb7d
lucastvasconcelos <lucastavaresvasconcelos@gmail.com>
2016-11-30 19:55:37 -0300

    pagina numero.html

--- appweb/index.js
+++ appweb/index.js
@@ -10,13 +10,15 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.post("/dosave", (req,res) => {
   console.log("os dados recebidos do formulário são:");
   console.log(req.body);
-  res.send(`<h1>Sucesso!</h1><ul><li>Seu nome: ${req.body.nome}</li>
-	<li>Endereço: ${req.body.endereco} </li>
-	<li>Telefone: ${req.body.telefone} </li>
-</ul>
-
-<a href='index.html'>voltar</a>`);
+  res.send(`<h1>Sucesso!</h1><ul><li>Seu nome: ${req.body.nome}</li><li>Endereço: ${req.body.endereco} </li><li>Telefone: ${req.body.telefone} </li></ul>
+<ahref='index.html'>voltar</a>`);
 });
 
-app.listen(3002);
+app.post("/adivinha",(req,res) =>{
+ 	console.log("numero: ");
+ 	console.log(req.body.numero);
+ 	res.send(`<h1> ${req.body.numero} </h1>`)
+});
+
+app.listen(3005);
 console.log("app online");
